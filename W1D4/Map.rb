6201d5df6c24291c5a3e8abcbbbde155e71f9349
Candra Tran <candratran@gmail.com>
2017-01-26 22:38:08 -0800

    Create Map class for 2D arrays

--- W1D4/Map.rb
+++ W1D4/Map.rb
@@ -1,20 +1,35 @@
 class Map
-
+  attr_accessor :store
 
   def initialize
-
+    @store = []
   end
 
   def assign(key,value)
-
+    store.each_with_index do |pair, i|
+      if pair[0] == key
+        store[i][1] = value
+        return pair
+      end
+    end
+    store << [key,value]
   end
   def lookup(key)
-
+    store.each do |pair|
+      if pair[0] == key
+        return pair
+      end
+    end
   end
-  def remove(key)
 
+  def remove(key)
+    store.each_with_index do |pair, i|
+      if pair[0] == key
+        store.delete_at(i)
+      end
+    end
   end
   def show
-
+    store
   end
 end
