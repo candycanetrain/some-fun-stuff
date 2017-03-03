1383770c1463bc9c8e489cf6db5d0e797555eed7
Candra Tran <candratran@gmail.com>
2017-03-03 07:27:51 -0800

    finished set up

---
+++ w6d5/skeleton/webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './index.jsx',
+  output: {
+    filename: './bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/], // Specifies file types to transpile
+        exclude: /(node_modules)/, // Leaves dependencies alone
+        loader: 'babel-loader', // Sets Babel as the transpiler
+        query: {
+          presets: ['es2015', 'react'] // Tells Babel what syntaxes to translate
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
